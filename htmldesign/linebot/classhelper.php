90bfe32516301ef2f3770591ed2ff8bcf9beba73
johnnylchang <riversnow@gmail.com>
2017-09-05 23:56:27 +0800

    should use https

--- htmldesign/linebot/classhelper.php
+++ htmldesign/linebot/classhelper.php
@@ -2,8 +2,8 @@
 require_once './include/composer.php';
 require_once './include/classredishelper.php';
 
-$res1 = "http://res.cloudinary.com/hiw54u1hl/image/upload/c_crop,h_300,w_1560,x_80,y_%d/v1504623069/class_schedule_hetios.png";
-$res2 = "http://res.cloudinary.com/hiw54u1hl/image/upload/c_crop,h_300,w_1560,x_80,y_%d/v1504623069/class_schedule_hetios.png";
+$res1 = "https://res.cloudinary.com/hiw54u1hl/image/upload/c_crop,h_300,w_1560,x_80,y_%d/v1504623069/class_schedule_hetios.png";
+$res2 = "https://res.cloudinary.com/hiw54u1hl/image/upload/c_crop,h_300,w_1560,x_80,y_%d/v1504623069/class_schedule_hetios.png";
 
 ini_set('memory_limit', '1024M');
 
