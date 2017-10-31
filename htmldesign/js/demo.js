1d147a6bb398d4ff961c43023249bbb18a74f4bc
LFChang <riversnow@gmail.com>
2017-10-31 21:45:20 +0800

    Push test for Heroku platform error

--- htmldesign/js/demo.js
+++ htmldesign/js/demo.js
@@ -4,7 +4,59 @@ $(document).ready(function () {
     $("#msgid").click(function () {
         alert("it is msgid");
     });
-    $('img').click(function () {
-        alert("it is image");
+    $("#float-img-bg>img").css("border", "3px solid red");
+    alert($('#float-img-bg').css('background-color'))
+
+    $('span').addClass('line');
+
+    $('span').click(function (e) {
+        $('#notMe').removeClass().addClass(e.target.id);
+        $('#notMe').removeClass('selected');
     });
+
+    $('#waypointsTable tr').hover(function() {
+        $(this).addClass('hover');
+    }, function() {
+        $(this).removeClass('hover');
+    });
+
+    function loop(e) {
+        e.animate({
+            top: "+=500"
+        }, 2000);
+        e.animate({
+            top: "-=500"
+
+        }, 2000, 'linear', function () {
+            loop(e);
+        });
+    }
+
+    function loop2(e) {
+        e.animate({
+            left: "+=500"
+        }, 2000);
+        e.animate({
+            left: "-=500"
+        }, 2000, 'linear', function () {
+            loop2(e);
+        });
+    }
+
+    function loop3(e) {
+        e.animate({
+            left: "+=500",
+            top: "+=500"
+        }, 2000);
+        e.animate({
+            left: "-=500",
+            top: "-=500"
+        }, 2000, 'linear', function () {
+            loop3(e);
+        });
+    }
+
+    loop($("#float-img-bg>img").eq(1));
+    loop2($("#float-img-bg>img").eq(0));
+    loop3($("#float-img-bg>img").eq(2));
 });
---
+++ php/hello.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// page1.php
+
+session_start();
+
+echo 'Welcome to page #1';
+
+$_SESSION['favcolor'] = 'green';
+$_SESSION['animal']   = 'cat';
+$_SESSION['time']     = time();
+
+// Works if session cookie was accepted
+echo '<br /><a href="hello2.php">page 2</a>';
+
+// Or maybe pass along the session id, if needed
+echo '<br /><a href="hello2.php?user=123">page 2</a>';
+?>
+<form>
+</form>
+</body>
+</html>
